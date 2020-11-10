38fe172e10dff998b977138bea58b06d48e030cd
Junu Kim <kjune0322@naver.com>
2020-11-10 10:36:02 +0900

    Implement Metadata continuous crwaling

--- Spider/write_csv.py
+++ Spider/write_csv.py
@@ -1,25 +1,31 @@
 import csv
 from spider import *
+import time
 
 def init_csv(app):
-    app.file = open(app.name+'.csv','w+')
-    app.wr = csv.writer(app.file, delimiter = '\t')
-    app.wr.writerow(['Name', app.name])
-    app.wr.writerow(['Address', PLAY_DOMAIN+app.addr])
+    app.reviewfile = open(app.name+'_review.tsv','w+')
+    app.wr = csv.writer(app.reviewfile, delimiter = '\t')
     
-def write_metadata(app):
-    app.wr.writerow(['Mean Rate', app.mean_rate])
-    app.wr.writerow(['Total Rates', app.tot_rates])
-    app.wr.writerow(['Rate 5', app.rate_5])
-    app.wr.writerow(['Rate 4', app.rate_4])
-    app.wr.writerow(['Rate 3', app.rate_3])
-    app.wr.writerow(['Rate 2', app.rate_2])
-    app.wr.writerow(['Rate 1', app.rate_1])
+    app.metafile = open(app.name+'_meta.tsv', 'w+')
+    app.metawr = csv.writer(app.reviewfile, delimiter = '\t')
+
+    app.metawr.writerow(['Name', app.name])
+    app.metawr.writerow(['Address', PLAY_DOMAIN+app.addr])
+    app.metawr.writerow(['Time', 'Mean Rate', 'Total Rates', 'Rate 5', 'Rate 4', 'Rate 3', 'Rate 2', 'Rate 1'])
+
+
+def write_metadata(app, t):
+    t = time.gmtime(t)
+    template = '{year}.{mon}.{day} {hour}:{min}'
+    template = template.format(year = t.tm_year, mon = t.tm_mon, day = t.tm_mday, hour = t.tm_hour, min = t.tm_min)
+    app.metawr.writerow([template, app.mean_rate, app.tot_rates, app.rate_5, app.rate_4, app.rate_3, app.rate_2, app.rate_1])
+
 
 def write_all_reviews(app):
-    app.wr.writerow(['Writer', 'Rate', 'Date', 'Contents'])
+    app.reviewwr.writerow(['Writer', 'Rate', 'Date', 'Contents'])
     for i in reversed(range(len(app.writer))):
-        app.wr.writerow([app.writer[i], app.rate[i], app.date[i], app.contents[i]])
+        app.reviewwr.writerow([app.writer[i], app.rate[i], app.date[i], app.contents[i]])
     
+
 def write_new_review(app, writer, rate, date, contents):
-    app.wr.writerow([writer, rate, date, contents])
+    app.reviewwr.writerow([writer, rate, date, contents])
