9153f24a79dfe960d1dc36e6f96dd29081094640
MinJae Song <52192602+songminjae@users.noreply.github.com>
2020-11-30 11:21:50 +0900

    show data

--- cs489/src/components/Appcard.tsx
+++ cs489/src/components/Appcard.tsx
@@ -1,13 +1,29 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Nav, Image } from "react-bootstrap";
+import { Modal, Button } from "react-bootstrap";
 
 import "../styles/css/Reviewcard.css";
 
 export function Appcard(props: { name: string }) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div className="maincontainercard">
-      <a>{props.name}</a>
+      <Button onClick={handleShow}>{props.name}</Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
